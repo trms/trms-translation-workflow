10e1b9281892ab5985715343262bb5f2d7e7b418
Seth Phillips <seth.phillips@trms.com>
2017-08-16 14:31:37 -0500

    try to get deps in line

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 /* eslint-env node */
 'use strict';
 
+const RSVP = require('rsvp');
+
 module.exports = {
   name: 'translation-workflow',
 
   isDevelopingAddon() {
     return true;
   },
+
   includedCommands(){
     return {
       'translate': require('./lib/commands/pre-translate'),
     }
   },
+
+  afterInstall: function(options) {
+    return RSVP.all([
+      this.addPackageToProject('flip-text'),
+      this.addPackageToProject('fs-extra'),
+      this.addPackageToProject('node-env-file'),
+    ])
+  }
 };
